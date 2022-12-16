c84e5a4dec8e2d507c6e38d37cb53b87cabdfc79
XCODE89 <maph0989@gmail.com>
2022-12-16 11:18:44 -0300

    fifth part

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -185,6 +185,9 @@ function esPositivo(numero) {
     if (numero<0) {
       return "Es negativo";
     }
+    else {
+      return false
+    }
 }
 
 function agregarSimboloExclamacion(str) {
@@ -205,7 +208,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return "Hola" + nombre + "!"
+  return "Hola " + nombre + "!"
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
@@ -218,14 +221,14 @@ function obtenerAreaRectangulo(alto, ancho) {
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  return 4*lado;
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+return base*altura/2;
 }
 
 
@@ -233,7 +236,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  return euro*1.2;
 }
 
 
@@ -243,9 +246,16 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  if (letra.length>1) {
+    return "Dato incorrecto"
+  }
+  else if (letra="a","e","i","o","u") {
+    return "Es vocal"
+   }
+   else {
+    return "Dato incorrecto"
+   }
 }
-
 
 
 // No modificar nada debajo de esta línea
