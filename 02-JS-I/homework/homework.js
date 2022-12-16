1c1ecd1af7e366499d46f7c1871cef045727e461
XCODE89 <maph0989@gmail.com>
2022-12-16 11:55:27 -0300

    sixth part

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -247,14 +247,15 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
   if (letra.length>1) {
-    return "Dato incorrecto"
+    return ("Dato incorrecto")
   }
-  else if (letra="a","e","i","o","u") {
-    return "Es vocal"
-   }
-   else {
-    return "Dato incorrecto"
-   }
+  else 
+    if (letra==="a" || letra==="e" || letra==="i" || letra==="o" || letra==="u") {
+      return ("Es vocal")
+    }
+    else {
+      return ("Dato incorrecto")
+    }
 }
 
 
